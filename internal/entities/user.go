46f40722568bdec4a93841c4af27c136a974d417
Darwin Fervil <dfervil@corp.free.fr>
2024-10-03 09:10:34 +0200

    ajout user struct

--- internal/entities/user.go
+++ internal/entities/user.go
@@ -1,1 +1,11 @@
 package entities
+
+type User struct {
+	ID         int    `json:"id"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	Email      string `json:"email"`
+	VerifiedAt string `json:"verified_at"`
+	CreatedAt  string `json:"created_at"`
+	UpdatedAt  string `json:"updated_at"`
+}
