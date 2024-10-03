35e1e3a6e85fcb6c6945cff1a7a3a16e23d5e361
Darwin Fervil <dfervil@corp.free.fr>
2024-10-03 09:32:03 +0200

    modification property type after review

--- internal/entities/user.go
+++ internal/entities/user.go
@@ -1,11 +1,13 @@
 package entities
 
+import "time"
+
 type User struct {
-	ID         int    `json:"id"`
-	Username   string `json:"username"`
-	Password   string `json:"password"`
-	Email      string `json:"email"`
-	VerifiedAt string `json:"verified_at"`
-	CreatedAt  string `json:"created_at"`
-	UpdatedAt  string `json:"updated_at"`
+	ID         int       `json:"id"`
+	Username   string    `json:"username"`
+	Password   string    `json:"-"`
+	Email      string    `json:"email"`
+	VerifiedAt time.Time `json:"verified_at"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
