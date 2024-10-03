34df211d91397983713d30c7cd555a4b9bdff1b3
Darwin Fervil <dfervil@corp.free.fr>
2024-10-03 11:18:29 +0200

    feat : create note controller function

--- internal/controller/notes.go
+++ internal/controller/notes.go
@@ -1,1 +1,34 @@
 package controller
+
+import (
+	"Dairanotes/internal/business"
+	"github.com/gin-gonic/gin"
+)
+
+type NotesController struct {
+	bn business.NoteBusinessInterface
+}
+
+func NewNotesController(bn business.NoteBusinessInterface) *NotesController {
+	return &NotesController{bn: bn}
+}
+
+func (nc *NotesController) CreateNote(c *gin.Context) {
+	// TODO
+}
+
+func (nc *NotesController) GetNotes(c *gin.Context) {
+	// TODO
+}
+
+func (nc *NotesController) GetNoteByID(c *gin.Context) {
+	// TODO
+}
+
+func (nc *NotesController) UpdateNoteByID(c *gin.Context) {
+	// TODO
+}
+
+func (nc *NotesController) DeleteNoteByID(c *gin.Context) {
+	// TODO
+}
